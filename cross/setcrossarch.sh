2a9f14f32adea3256ff566a4d285bf4fd15af11d
codehelp <codehelp@563faec7-e20c-0410-992a-a66f704d0ccd>
2010-05-03 18:44:41 +0000

    ensure dpkg-cross is available inside the chroot
    git-svn-id: http://www.emdebian.org/svn/current/host/trunk/multistrap/trunk@7175 563faec7-e20c-0410-992a-a66f704d0ccd

--- cross/setcrossarch.sh
+++ cross/setcrossarch.sh
@@ -47,10 +47,9 @@ fi
 # set the value inside the chroot
 echo "#!/bin/sh" > $dir/tmp/set.sh
 echo >> $dir/tmp/set.sh
-echo "export DEBIAN_FRONTEND=noninteractive ".
-	"DEBCONF_NONINTERACTIVE_SEEN=true ".
-	"LC_ALL=C LANGUAGE=C LANG=C" >> $dir/tmp/set.sh
+echo "export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=C LANGUAGE=C LANG=C" >> $dir/tmp/set.sh
 echo "echo debconf dpkg-cross/default-arch select $CROSSARCH|debconf-set-selections" >> $dir/tmp/set.sh
 echo "dpkg-reconfigure dpkg-cross" >> $dir/tmp/set.sh
+echo "Setting debconf dpkg-cross/default-arch to $CROSSARCH"
 sudo chroot $dir sh /tmp/set.sh
 sudo rm $dir/tmp/set.sh
