2c3e20b73313fbe8054a5ed7853ef67977d3b8de
codehelp <codehelp@563faec7-e20c-0410-992a-a66f704d0ccd>
2011-01-29 15:45:07 +0000

    Add sysvinit method to existing upstart support in script for native chroots to disable starting daemons. (Closes: #611188)
    git-svn-id: http://www.emdebian.org/svn/current/host/trunk/multistrap/trunk@7726 563faec7-e20c-0410-992a-a66f704d0ccd

--- examples/chroot.sh
+++ examples/chroot.sh
@@ -2,36 +2,40 @@
 
 set -e
 
-# This setup script is an alternative method of adjusting the tarball
-# contents immediately after multistrap has unpacked the packages.
-
-# At this stage, any operations inside the rootfs must not try to
-# execute any binaries within the rootfs.
-
 # The script is called with the following arguments:
-
 # $1 = $DIR  - the top directory of the debootstrapped system
 # $2 = $ARCH - the specified architecture, already checked with dpkg-architecture.
-
 # setup.sh needs to be executable.
 
 TARGET=$1
-
+# upstart support
 if [ -x "$TARGET/sbin/initctl" ]; then
-  mv "$TARGET/sbin/start-stop-daemon" "$TARGET/sbin/start-stop-daemon.REAL"
-  echo \
+	echo "initctl: Trying to prevent daemons from starting in $TARGET"
+	mv "$TARGET/sbin/start-stop-daemon" "$TARGET/sbin/start-stop-daemon.REAL"
+	echo \
 "#!/bin/sh
 echo
 echo echo \"Warning: Fake start-stop-daemon called, doing nothing\"" > "$TARGET/sbin/start-stop-daemon"
-  chmod 755 "$TARGET/sbin/start-stop-daemon"
+	chmod 755 "$TARGET/sbin/start-stop-daemon"
 fi
-            
 if [ -x "$TARGET/sbin/initctl" ]; then
-    mv "$TARGET/sbin/initctl" "$TARGET/sbin/initctl.REAL"
-    echo \
+	echo "initctl: Trying to prevent daemons from starting in $TARGET"
+	mv "$TARGET/sbin/initctl" "$TARGET/sbin/initctl.REAL"
+	echo \
 "#!/bin/sh
 echo
 echo \"Warning: Fake initctl called, doing nothing\"" > "$TARGET/sbin/initctl"
-  chmod 755 "$TARGET/sbin/initctl"
+	chmod 755 "$TARGET/sbin/initctl"
 fi
-	  
+
+# sysvinit support - exit value of 101 is essential.
+if [ -x "$TARGET/sbin/init" && ! -f "$TARGET/usr/sbin/policy-rc.d" ]; then
+	echo "sysvinit: Using policy-rc.d to prevent daemons from starting in $TARGET"
+	mkdir -p $TARGET/usr/sbin/
+	cat > $TARGET/usr/sbin/policy-rc.d << EOF
+#!/bin/sh
+echo "sysvinit: All runlevel operations denied by policy" >&2
+exit 101
+EOF
+	chmod a+x $TARGET/usr/sbin/policy-rc.d
+fi
